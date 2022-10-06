5177c0ea4066c46893468571ce640dd30e92c16b
g-prog <alaobukky2@gmail.com>
2022-10-06 19:03:09 +0100

    making progress with the hero section

--- niyo-app/src/App.js
+++ niyo-app/src/App.js
@@ -29,6 +29,31 @@ function App() {
           </ButtonDiv>
         </RightNav>
       </Nav>
+      <Body>
+        <BodyLeft>
+          <h1>
+            Discover amazing <br /> art around the <span>world</span>{" "}
+          </h1>
+          <TextContent>
+            <p>
+              Browse a curated selection of art around the world, including
+              museum exhibitions, gallery openings, upcoming and many more
+            </p>
+          </TextContent>
+
+          <ExploreArtsContent>
+            <ExploreTop>Explore Arts</ExploreTop>
+            <ExploreBottom>Hello</ExploreBottom>
+          </ExploreArtsContent>
+        </BodyLeft>
+
+        <BodyRight>
+          <SpiralBottom>
+            <img src="/assets/spiral-bottom.png" alt="spiral" />
+          </SpiralBottom>
+        </BodyRight>
+        
+      </Body>
     </Container>
   );
 }
@@ -37,6 +62,35 @@ export default App;
 
 const Container = styled.div`
   color: white;
+`;
+
+const Body = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const SpiralBottom = styled.div`
+  
+`;
+
+const BodyRight = styled.div`
+  
+`;
+
+const BodyLeft = styled.div`
+  h1 {
+    font-weight: 700;
+    font-size: 68px;
+    color: #ffffff;
+  }
+
+  span {
+    color: #fbaf00;
+  }
+`;
+
+const TextContent = styled.div`
+  width: 45%;
 `;
 
 const Nav = styled.div`
@@ -79,6 +133,15 @@ const ExploreTop = styled.div`
   /* height: 20px; */
 `;
 
+
+const ExploreArtsContent = styled.div`
+  cursor: pointer;
+  padding: 20px 10px;
+  width:15%;
+`;
+
+
+
 const Topstack = styled.div`
   background: #fbaf00;
   border-radius: 0px 30px;
@@ -116,5 +179,3 @@ const RightNav = styled.div`
 `;
 
 const ImageContainer = styled.div``;
-
-
